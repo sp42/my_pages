f7fa1de9497dbe4b09545f066074ee321a9b6d3d
Frank <sp42@qq.com>
2023-12-28 01:03:54 +0800

    ci

--- docs/docs/common/inner.js
+++ docs/docs/common/inner.js
@@ -59,7 +59,6 @@ function highlightDDLKeywords(text, isSqlDDL) {
     var highlightedText = '';
 
     words.forEach(function (word) {
-
         if ((isSqlDDL ? ddlKeywords : sqlKeywords).includes(word.trim().toUpperCase()))
             highlightedText += '<span class="sql-ddl-keyword">' + word + '</span> ';
         else
@@ -69,8 +68,19 @@ function highlightDDLKeywords(text, isSqlDDL) {
     return highlightedText;
 }
 
+
+// Java 关键字
+const javaKeywordsRegex = /\b(new|return|public|static|class|void|int|boolean|if|else|while|for|switch|case|break|default|try|catch|finally|throw|throws|private|protected|package|import|interface|implements|extends|this|super|instanceof|final|abstract|synchronized|volatile)\b/g;
+const matchJavaAnn = /@\w+/;
+
 const matchString = /'[^']*'/g;
+const matchString2 = /"[^"]*"/g;
 const matchSqlField = /`[^`]*`/g;
+
+// 匹配 YAML 键名
+const keyRegex = /^(\s*)(\w+)(\s*):/gm;
+// 匹配 YAML 字符串值
+const valueRegex = /(['"])(.*?)\1/g;
 
 function highlightCode() {
     var codeElement = document.querySelectorAll('.xml-code');
@@ -103,7 +113,43 @@ function highlightCode() {
 
         item.innerHTML = highlightDDLKeywords(code);
     });
+
+    codeElement = document.querySelectorAll('.java-code');
+
+    codeElement && codeElement.length && codeElement.forEach(item => {
+        let code = item.innerHTML;
+        code = code.replace(matchString2, m => `<span Class="code-string">${m}</span>`);
+        code = code.replace(javaKeywordsRegex, '<span class="java-keyword">$1</span>');
+        code = code.replace(matchJavaAnn, m => `<span class="java-anno">${m}</span>`);
+
+        item.innerHTML = code;
+    });
+
+    codeElement = document.querySelectorAll('.yaml-code');
+
+    codeElement && codeElement.length && codeElement.forEach(item => {
+        let code = item.innerHTML;
+
+        const yamlKeywords = ["true", "false", "null"];
+
+        // YAML语法规则的正则表达式
+        const regex = /(:|\s+|-|#.*$|".*"|'.*'|\d+|\btrue\b|\bfalse\b|\bnull\b)/g;
+
+        // 将文本中的关键字和字符串用<span>标签包裹起来
+        code = code.replace(regex, function (match) {
+            if (yamlKeywords.includes(match))
+                return '<span class="yaml-keyword">' + match + '</span>';
+            else if (match.startsWith('#'))
+                return '<span class="yaml-comment">' + match + '</span>';
+            else if (match.startsWith('"') || match.startsWith("'"))
+                return '<span class="yaml-string">' + match + '</span>';
+            else
+                return match;
+        });
+
+        item.innerHTML = code;
+    });
 }
 
 // 页面加载完成后，调用 highlightCode 函数
-window.onload = highlightCode;
+document.addEventListener('DOMContentLoaded', highlightCode);
